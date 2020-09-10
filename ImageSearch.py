00f69af488eb8d562bb3e85dd8c365418e35c7a6
Defcon27 <defcon.sentinal95@gmail.com>
2020-09-10 23:08:22 +0530

    reading images from path of image database

--- ImageSearch.py
+++ ImageSearch.py
@@ -1,7 +1,29 @@
 import os
-import PIL
+import time
+import cv2
+import matplotlib.pyplot as plt
 
 
-newdir = os.getcwd()+"/Image_Database"
-for dir in os.listdir(newdir):
-    print(dir)
+image_db_path = "Image_Database/"
+for img in os.listdir(image_db_path):
+
+    image = cv2.imread(image_db_path+img)
+    image = cv2.resize(image, (1000, 1000))
+    image_y = image.shape[0]
+    image_x = image.shape[1]
+
+    # plt.imshow(image)
+    # plt.show()
+
+    tot_inten = 0
+    for y in range(image_y):
+        for x in range(image_x):
+            #print(image[y, x])
+            tot_inten += image[y, x][1]
+
+    print(tot_inten/(image_y*image_x))
+
+
+# img = cv2.imread("Image_Database/100000.jpg")
+# plt.imshow(img)
+# plt.show()
