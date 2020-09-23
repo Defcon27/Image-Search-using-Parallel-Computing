235666562c95ca2f09afbdec16744e8c2f396787
Defcon27 <defcon.sentinal95@gmail.com>
2020-09-23 11:21:16 +0530

    convolution filters class

---
+++ ConvolutionalFilters.py
@@ -0,0 +1,48 @@
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class ConvolutionFilter():
+
+    def __init__(self, image):
+        self.image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        self.sobelX = np.array([[-1, 0, 1],
+                                [-2, 0, 2],
+                                [-1, 0, 1]])
+
+        self.sobelY = np.array([[-1, -2, -1],
+                                [0, 0, 0],
+                                [1, 2, 1]])
+
+    def __convolution(self, image_roi, kernel):
+        kernel_dimension = len(kernel)
+        pixel_sum = 0
+
+        for i in range(kernel_dimension):
+            for j in range(kernel_dimension):
+                pixel_kernel_value = image_roi[i, j]*kernel[i, j]
+                pixel_sum = pixel_sum+pixel_kernel_value
+
+        if pixel_sum < 0:
+            return 0
+        else:
+            return pixel_sum % 255
+
+    def applyFilter(self, kernel):
+        image = self.image
+        filtered_image = np.zeros(image.shape)
+
+        for row in range(1, len(image)-1):
+            for col in range(1, len(image[row])-1):
+
+                pixels = image[row-1:row+2, col-1:col+2]
+                pixel_kernel = (pixels * kernel).sum()
+                if pixel_kernel > 0:
+                    filtered_image[row, col] = pixel_kernel % 255
+                else:
+                    filtered_image[row, col] = 0
+
+        return filtered_image
