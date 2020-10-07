24f87fd44b7709b86b5cb472d39c9aa87edcbffa
Defcon27 <defcon.sentinal95@gmail.com>
2020-10-07 17:26:48 +0530

    Threaded Image Search implementation

--- ImageSearch_parallel.py
+++ ImageSearch_parallel.py
@@ -1,10 +1,12 @@
 import os
+import sys
 import threading
 import concurrent.futures
 import time
 import cv2
 import matplotlib.pyplot as plt
 from FeatureVectors import FeatureVectors
+from QuerySearch import QuerySearch
 
 
 def extractFeatureVectors(image_path):
@@ -32,7 +34,15 @@ def ThreadedFeatureExtraction(images_list):
     return features
 
 
-if __name__ == "__main__":
+def getImg(img):
+    image_db_path = "Image_Database/"
+    image = cv2.imread(image_db_path+img)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    return image
+
+
+# if __name__ == "__main__":
+def ImageSearch(queryImage):
 
     start = time.perf_counter()
 
@@ -41,13 +51,16 @@ if __name__ == "__main__":
     for img in os.listdir(image_db_path):
         image_paths.append(image_db_path+img)
 
-    lists = [image_paths, image_paths, image_paths, image_paths]
-
-    # for image in image_paths:
-    #     print(extractFeatureVectors(image))
-
     features = ThreadedFeatureExtraction(image_paths)
-    print(features)
 
-    end = time.perf_counter()
-    print(f"Time : {end-start}")
+    # queryImage = sys.argv[1]
+    queryImage_path = image_db_path+queryImage
+
+    imageName, queryVector = extractFeatureVectors(queryImage_path)
+
+    search = QuerySearch(queryVector, features)
+    results = search.performSearch()
+
+    results.sort(key=lambda res: res[1])
+
+    return results
